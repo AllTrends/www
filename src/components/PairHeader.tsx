a9e5521856e24acd3e1c0a3bd7adaa92d3fd49b1
AmandineBI <amandine7moi92@hotmail.com>
2023-10-24 20:27:44 +0200

    Improve PairHeader

--- src/components/PairHeader.tsx
+++ src/components/PairHeader.tsx
@@ -1,3 +1,4 @@
+import { ChevronDown } from "lucide-react";
 import React, { useState } from "react";
 import type { Pair } from "~/types";
 
@@ -9,59 +10,75 @@ interface PairHeaderProps {
 const PairHeader = (props:PairHeaderProps) => {
     const [showPairs, setShowPairs] = useState(false);
 
+    const price = getPrice(props.pair.numerator, props.pair.denominator);
+    const change = getChange(props.pair.numerator, props.pair.denominator);
+    const high = getHigh(props.pair.numerator, props.pair.denominator);
+    const low = getLow(props.pair.numerator, props.pair.denominator);
+    let classHidden = "";
+
     function getPrice(numerator: string, denominator: string) {
-        console.log(`Get price for ${numerator}Perp/${denominator} pair.`)
-        return 1000;
+        console.log(`Get price for ${numerator}Perp/${denominator} pair.`);
+        const _price = 1000; // Call API
+        return new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD'}).format(_price);
     }
 
     function getChange(numerator: string, denominator: string) {
-        console.log(`Get 24h Change for ${numerator}Perp/${denominator} pair.`)
-        return "+5%";
+        console.log(`Get 24h Change for ${numerator}Perp/${denominator} pair.`);
+        const _change = 0.0564; // Call API
+        if (_change < 0) {
+            return (_change*100).toFixed(2)+"%";
+        } else {
+            return "+" + (_change*100).toFixed(2)+"%";
+        }
     }
 
     function getHigh(numerator: string, denominator: string) {
-        console.log(`Get 24h High for ${numerator}Perp/${denominator} pair.`)
-        return 1200;
+        console.log(`Get 24h High for ${numerator}Perp/${denominator} pair.`);
+        const _high = 1200; // Call API
+        return new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD'}).format(_high);
     }
 
     function getLow(numerator: string, denominator: string) {
-        console.log(`Get 24h Low for ${numerator}Perp/${denominator} pair.`)
-        return 800;
+        console.log(`Get 24h Low for ${numerator}Perp/${denominator} pair.`);
+        const _low = 800; // Call API
+        return new Intl.NumberFormat('us-US', { style: 'currency', currency: 'USD'}).format(_low);
     }
 
     function togglePairs() {
         setShowPairs(prevShowPairs => !prevShowPairs);
         console.log(showPairs);
+        classHidden = showPairs? "" : "hidden";
+        console.log(classHidden);
     }
 
     return (
-        <div className="container mx-auto p-5 w-full flex space-x-4 text-white">
-            <div className="static">
-                <button className="font-bold shadow hover:shadow-xl static flex"
+        <div className="container mx-auto p-5 w-full flex items-center justify-start gap-12 text-white">
+            <div className="relative">
+                <button className="font-bold shadow hover:shadow-xl flex"
                         onClick={togglePairs}>
-                    <div className="h-full text-xl">{props.pair.numerator}Perp/{props.pair.denominator} pair</div> 
-                    <div className="h-full align-bottom mt-2 mb-0 pl-3">&#65088;</div>
+                    <div className="h-full text-xl">{props.pair.numerator}Perp/{props.pair.denominator}</div> 
+                    <ChevronDown className="ml-2" aria-hidden="true"/>
                 </button>
-
-                <div className={"absolute bottom-0 left-0 " + showPairs?"":"collapse"}>
-                    This is the list of pairs.
+                <div className={`absolute mt-3 pb-20 pt-2 pl-2 flex flex-col bg-black bg-opacity-90 min-w-[160px] rounded-md text-lg ${showPairs ? "" : "hidden"}`}>
+                    Other pairs soon to come...
                 </div>
             </div>
+
             <div>
-                Price
-                {getPrice(props.pair.numerator, props.pair.denominator)}
+                <div className="font-semibold">Price</div>
+                <div>{price}</div>
             </div>
             <div>
-                24h Change
-                {getChange(props.pair.numerator, props.pair.denominator)}
+                <div className="font-semibold">24h Change</div>
+                <div className={change.startsWith("-") ? "text-[#eb4034]" : "text-[#49cc10]"}>{change}</div>
             </div>
             <div>
-                24h High
-                {getHigh(props.pair.numerator, props.pair.denominator)}
+                <div className="font-semibold">24h High</div>
+                <div>{high}</div>
             </div>
             <div>
-                24h Low
-                {getLow(props.pair.numerator, props.pair.denominator)}
+                <div className="font-semibold">24h Low</div>
+                <div>{low}</div>
             </div>
         </div>
     );
