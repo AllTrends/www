e138635593e695f4c577e016768ba89d13331c4d
Loa212 <anobileloris@gmail.com>
2023-10-24 15:36:57 +0200

    general layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,12 @@
-import AutoanimateTest from "~/components/AutoanimateTest";
-import { Button } from "~/components/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "~/components/ui/sheet";
-
 export default function Home() {
   // const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <main className="flex flex-col items-start justify-start gap-8">
-      Hello
-      <Button variant={"destructive"}>Hello</Button>
+    <main className="container mx-auto mb-8 mt-4 grid w-full grow grid-cols-4 gap-4">
+      <div className="col-span-3 ring ring-red-700">
+        <h1></h1>
+      </div>
+      <div className="col-span-1 ring ring-green-700"></div>
     </main>
   );
 }
