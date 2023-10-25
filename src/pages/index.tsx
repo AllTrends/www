1cca377a00bd4de982722d0b8c98f42d4ea8a155
Loa212 <anobileloris@gmail.com>
2023-10-25 10:38:02 +0200

    format currency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,25 +25,73 @@ export default function Home() {
 }
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const TradingUi = () => {
+  const {
+    address,
+    isConnecting: accountLoading,
+    isDisconnected: accountDisconnected,
+  } = useAccount();
+
+  const {
+    data: balance,
+    isError: balanceError,
+    isLoading: balanceLoading,
+  } = useBalance({
+    address,
+  });
+
+  const isLoading = accountLoading || balanceLoading;
+
+  if (isLoading)
+    return (
+      <div className="flex h-full flex-col items-center justify-start gap-6 rounded-md bg-stone-900 p-6 ring-1 ring-stone-500 ">
+        <h3 className="text-xl">Loading account data</h3>
+        <p className="text-center text-stone-300">
+          This can take a few seconds, please wait...
+        </p>
+        <Loader2 className="h-8 w-8 animate-spin" />
+      </div>
+    );
+
+  if (accountDisconnected)
+    return (
+      <div className="flex h-full flex-col items-center justify-start gap-6 rounded-md bg-stone-900 p-6 ring-1 ring-stone-500 ">
+        <h3 className="text-lg">Account not connected</h3>
+        <div className="rounded-md ring-1 ring-stone-500 hover:ring-0">
+          <ConnectButton chainStatus={"icon"} />
+        </div>
+      </div>
+    );
+
+  if (balanceError)
+    return (
+      <div className="flex h-full flex-col items-center justify-start gap-6 rounded-md bg-stone-900 p-6 ring-1 ring-stone-500 ">
+        <h3 className="text-lg">Error loading balance</h3>
+        <p className="text-center text-stone-300">Please try again later</p>
+      </div>
+    );
+
   return (
-    <Tabs defaultValue="buy">
-      <TabsList className="w-full ring-1 ring-stone-500">
-        <TabsTrigger className="grow" value="buy">
-          Buy
-        </TabsTrigger>
-        <TabsTrigger className="grow" value="sell">
-          Sell
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="buy" className="min-h-[60vh]">
-        <Buy />
-      </TabsContent>
-      <TabsContent value="sell">
-        <Sell />
-      </TabsContent>
-    </Tabs>
+    <div className="flex h-full flex-col items-center justify-start gap-6 rounded-md bg-stone-900 ring-1 ring-stone-500 ">
+      <Tabs defaultValue="buy" className="w-full">
+        <TabsList className="w-full">
+          <TabsTrigger className="grow" value="buy">
+            Buy
+          </TabsTrigger>
+          <TabsTrigger className="grow" value="sell">
+            Sell
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="buy" className="min-h-[60vh]">
+          <Buy balance={balance} />
+        </TabsContent>
+        <TabsContent value="sell">
+          <Sell />
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 };
 import {
@@ -62,18 +110,16 @@ import { defaultPair } from "~/utils/constants";
 import useTradesStore from "~/stores/tradesStore";
 import { type ExecutedTrade } from "~/types";
 import { useAccount, useBalance } from "wagmi";
+import { Loader2 } from "lucide-react";
+import { Separator } from "~/components/ui/separator";
+import React from "react";
 
-const Buy = () => {
+const Buy = ({
+  balance,
+}: {
+  balance: ReturnType<typeof useBalance>["data"];
+}) => {
   const addTransaction = useTradesStore((state) => state.addTrade);
-  const { address, isConnecting, isDisconnected } = useAccount();
-
-  const {
-    data: balance,
-    isError: balanceError,
-    isLoading: balanceLoading,
-  } = useBalance({
-    address,
-  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -96,27 +142,51 @@ const Buy = () => {
   };
 
   const setMaxAmount = () => {
-    console.log(balance);
     if (!balance) return;
-    const input = document.getElementById("amount") as HTMLInputElement;
-    alert(balance.formatted);
+    setAmount(balance.formatted);
+  };
+
+  const [amount, setAmount] = React.useState("");
+  const [disabled, setDisabled] = React.useState(false);
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // this need to be formatted as a number with decimals separated by a dot
+    const value = e.target.value;
+    const regEx = /^[0-9]+(\.[0-9]{0,2})?$/;
+    if (value === "" || regEx.test(value)) {
+      // check if the amount is more than the balance
+      if (balance && parseFloat(value) > parseFloat(balance.formatted)) {
+        // if it is disable the buy btn
+        setDisabled(true);
+      } else {
+        // enable the buy btn
+        setDisabled(false);
+      }
+      setAmount(value);
+    }
   };
 
   return (
     <form
-      className="flex min-h-[60vh] flex-col items-start justify-start gap-4 rounded-md bg-stone-900 p-4 pb-8 ring-1 ring-stone-500"
+      className="relative flex  flex-col items-start justify-start gap-4 p-4"
       onSubmit={handleSubmit}
     >
       <div className="w-full grow">
-        <h3 className="mb-6 text-2xl">Buy</h3>
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="amount">Amount</Label>
-          <Input min={0} type="number" id="amount" placeholder="Amount" />
+          <Input
+            className="text-end"
+            id="amount"
+            type="text"
+            placeholder="0.00"
+            value={amount}
+            onChange={handleAmountChange}
+          />
+
           <Label asChild className="-mt-1.5 place-self-end" htmlFor="amount">
             <Button
               type="button"
               onClick={setMaxAmount}
-              disabled={balanceLoading}
               size={"sm"}
               variant={"link"}
             >
@@ -125,9 +195,26 @@ const Buy = () => {
           </Label>
         </div>
       </div>
-      <Button className="w-full" type="submit">
-        Buy
+      <Button
+        disabled={disabled}
+        className="w-full bg-green-300 hover:bg-green-200"
+        type="submit"
+      >
+        Buy / Long
       </Button>
+      <Separator className="bg-stone-200/40" />
+      <div className="w-full grow text-sm text-stone-300">
+        <ul className="space-y-2">
+          <li className="flex items-center justify-between">
+            <span>Spread:</span>
+            <span>3.50%</span>
+          </li>
+          <li className="flex items-center justify-between">
+            <span>Volume:</span>
+            <span>10000000</span>
+          </li>
+        </ul>
+      </div>
     </form>
   );
 };
